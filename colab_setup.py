3344b5399a4d544c9d00ebe43648ec8903cab0f7
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2019-09-16 14:25:50 -0500

    use shutil rm

--- colab_setup.py
+++ colab_setup.py
@@ -1,6 +1,11 @@
 import os
+import shutil
+from google.colab import drive
 
-os.rmdir("energyplus")
+drive.mount('/content/drive')
+
+shutil.rmtree("eplusparser", ignore_errors=True)
 os.system("git clone https://github.com/energy-plus-senior-design/eplusparser.git")
+
 os.chdir("eplusparser")
 os.system("pip install .")
