bc774bb0a47615c3a985735aac85b6128630b1eb
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2019-09-16 14:20:10 -0500

    use os.chdir

--- colab_setup.py
+++ colab_setup.py
@@ -1,10 +1,6 @@
 import os
-from google.colab import drive
 
-drive.mount('/content/drive')
-
-os.system("rm -rf eplusparser && git clone https://github.com/energy-plus-senior-design/eplusparser.git")
-
-os.system("cd eplusparser")
-os.system("pwd")
+os.rmdir("energyplus")
+os.system("git clone https://github.com/energy-plus-senior-design/eplusparser.git")
+os.chdir("eplusparser")
 os.system("pip install .")
